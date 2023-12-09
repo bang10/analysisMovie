50cc55445f4d3f8e7fbe645258cc3c592caa397f
bang10 <bang369953@gmail.com>
2023-12-10 00:31:53 +0900

    흥행 여부 판단

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ whetherMovieBoxOfficeList = {}
 
 # README 2번 흥행 여부
 def isBoxOffice(visitor) :
+    if visitor == 0:
+        return 'Movie Error'
+
     return True if visitor > 500000 else False
 
 # 이상한 것도 같이 크롤링 되는 것 방지
@@ -28,6 +31,41 @@ def chageDataOnlyNumber(param):
             return float(param) if '.' in param else param
 
 
+def movieBaseInfo(parser):
+    movieBaseInfo = {}
+    # 영화 이름 detail_tit > tit_movie > span .txt_tit
+    titleTag = parser.find('span', {'class': 'txt_tit'})
+    movieTitle = titleTag.text
+    movieBaseInfo['movieTitle'] = movieTitle
+
+    # 누적 관람객이 없는 김에 모두 예외처리
+    try:
+        # 장르
+        genre = parser.find('dt', text='장르').find_next('dd').get_text(strip=True)
+        movieBaseInfo['genre'] = genre
+    except:
+        genre = 'genre is not found'
+        movieBaseInfo['genre'] = genre
+
+    try:
+        # 영화 별점
+        score = parser.find('dt', text='평점').find_next('dd').get_text(strip=True)
+        movieBaseInfo['score'] = score
+    except:
+        score = 0
+        movieBaseInfo['score'] = score
+
+    # 누적 관람객이 없는 경우가 있는 경우 대비 ex)40778
+    try:
+        # 누적 관람객
+        visitor = parser.find('dt', text='누적관객').find_next('dd').get_text(strip=True)
+        movieBaseInfo['visitor'] = visitor
+    except:
+        visitor = 0
+        movieBaseInfo['visitor'] = visitor
+
+    return movieBaseInfo
+
 def getReviewInfo(baseUrl, movieIdList):
     for movieId in movieIdList:
         movieUrl = baseUrl + movieId
@@ -35,42 +73,34 @@ def getReviewInfo(baseUrl, movieIdList):
         sourceType = response.text
         parser = BeautifulSoup(sourceType, 'html.parser')
 
-        # 영화 이름 detail_tit > tit_movie > span .txt_tit
-        titleTag = parser.find('span', {'class': 'txt_tit'})
-        movieTitle = titleTag.text
+        # 영화 기본 정보
+        movieInfo = movieBaseInfo(parser)
 
-        # 영화 평점 info_detail > detail_cont > list_cont >> dd
-        movieScoreTag = parser.select('.list_cont dd')
-        movieScore = float(chageDataOnlyNumber(movieScoreTag[6].text))
-        # 관람객 수
-        print(movieScoreTag)
-        try:
-            visitor = chageDataOnlyNumber(movieScoreTag[7].text)
-        except :
-            visitor = chageDataOnlyNumber(movieScoreTag[6].text)
+        # 영화 리뷰 리스트
+
         # 흥행 여부
-        boxOffice = isBoxOffice(int(visitor))
-        whetherMovieBoxOfficeList[movieTitle] = boxOffice
+        boxOffice = isBoxOffice(chageDataOnlyNumber(movieInfo['visitor']))
+        whetherMovieBoxOfficeList[movieInfo['movieTitle']] = boxOffice
+
         # 관람객 ID 정보
         # TODO
-        visitorTag = parser.select('p')
-        # print(visitorTag)
-        # 관람객 아이디 당 평점
+        # visitorTag = parser.select('p')
 
-        # 관람평
+
 
 
 def main():
     baseUrl = 'https://movie.daum.net/moviedb/grade?movieId='
-    movieIdList = ['39519', '3791', '35497', '40878', '43729', '44085', '46121', '145342', '1660', '109512',
-                   '160244', '122749', '104209', '43172', '76384', '100237', '89720', '69737', '44832', '139237',
-                   '3649', '40778', '41951', '62708', '43584', '111292', '79544', '163777', '115601', '53080']
+    # movieIdList = ['39519', '3791', '35497', '40878', '43729', '44085', '46121', '145342', '1660', '109512',
+    #                '160244', '122749', '104209', '43172', '76384', '100237', '89720', '69737', '44832', '139237',
+    #                '3649', '62730', '41951', '62708', '43584', '111292', '79544', '163777', '115601', '53080']
 
     # 테스트 코드, 실제 제출할 때는 위의 리스트로 수정
-    # movieIdList = ['39519']
+    movieIdList = ['39519']
 
+    # 2번 흥행 여부
     getReviewInfo(baseUrl, movieIdList)
-    print('영화별 흥행 여부 (관람객 50만명 이상)', whetherMovieBoxOfficeList)
+    print('영화별 흥행 여부 (관람객 50만명 이상)\n', whetherMovieBoxOfficeList)
 
 
 if __name__ == '__main__':
